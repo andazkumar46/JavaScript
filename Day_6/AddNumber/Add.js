afba5c79c61de8a44a22dd07d82a70dca00aa999
Andaz Kumar <andazkumar46@gmail.com>
2024-09-17 11:07:31 +0530

    add new projetc to find addition

---
+++ Day_6/AddNumber/Add.js
@@ -0,0 +1,7 @@
+const addnum = () => {
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
+    // let sum = num1 + num2;
+    document.getElementById("answer").innerHTML = `The Answer of num1 and num2 =  ${num1 **= num2}`
+
+}
